c0a17185c42fdc8af9849d3bbfb9852649711512
nimohe <nimohe@vip.qq.com>
2022-03-07 21:17:21 +0800

    update:sunny-land mobile

---
+++ sunny-land/web-mobile/src/settings.afcb1.js
@@ -0,0 +1,65 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "wall",
+        "player",
+        "enemy",
+        "item",
+        "gameUI"
+    ],
+    collisionMatrix: [
+        [
+            true,
+            null,
+            null,
+            false
+        ],
+        [
+            false,
+            false,
+            true,
+            true
+        ],
+        [
+            false,
+            true,
+            false,
+            true,
+            true
+        ],
+        [
+            false,
+            true,
+            true,
+            false
+        ],
+        [
+            false,
+            false,
+            true,
+            false,
+            false
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/Scene/start.fire",
+    orientation: "landscape",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "cb4f5",
+        main: "ee4dc"
+    }
+};
